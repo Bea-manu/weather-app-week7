357a8734721b8885db19f4653d2b0ac31e7eb184
Bea-manu <136429203+Bea-manu@users.noreply.github.com>
2023-11-11 16:21:04 +0200

    added event listener for form

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+function handlesearchSubmit(event){
+    event.preventDefault();
+    let searchInput = document.querySelector("#search-form-input");
+    let cityElement = document.querySelector("#city");
+    cityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form-input");
+searchFormElement.addEventListener("submit",handlesearchSubmit);
+
 function searchcity(city){
     let apiKey="8f0ab40o57b12e2t3b934b3b4137cfbc";
     let apiUrl="https://api.shecodes.io/weather/v1/current?query=${city}&key=${8f0ab40o57b12e2t3b934b3b4137cfbc}$unit=metric";
